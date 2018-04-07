46af20d5d8f4a536f5e9fee90d646e39c2111c7a
Luke Edwards <luke.edwards05@gmail.com>
2018-04-07 15:21:21 -0700

    update builder; use rollup & add umd

--- builder.js
+++ builder.js
@@ -1,24 +1,46 @@
-const fs = require('fs');
-const mkdir = require('mk-dirs');
-const { resolve } = require('path');
+const { rollup } = require('rollup');
+const { readFileSync } = require('fs');
 const { minify } = require('uglify-js');
-const imports = require('rewrite-imports');
 const pretty = require('pretty-bytes');
 const sizer = require('gzip-size');
 const pkg = require('./package');
 
-let data = fs.readFileSync('src/index.js', 'utf8');
+let input = 'src/index.js';
 
-mkdir('dist').then(_ => {
-	// Copy as is for ESM
-	fs.writeFileSync(pkg.module, data);
+let buble = require('rollup-plugin-buble')();
+let resolve = require('rollup-plugin-node-resolve')();
+let uglify = toplevel => {
+	return require('rollup-plugin-uglify')({
+		output: { comments:false },
+		compress: { keep_infinity:true, pure_getters:true },
+		warnings: true,
+		toplevel,
+		ecma: 5
+	});
+}
 
-	// Mutate imports for CJS
-	data = imports(data).replace(/export default/, 'module.exports =');
-	fs.writeFileSync(pkg.main, data);
+function config(file, format, opts={}) {
+	let { name, external, plugins } = opts;
+	return rollup({ input, plugins, external }).then(bun => {
+		return bun.write({ format, name, file, strict:false });
+	});
+}
 
-	// Uglify & print gzip size
-	const { code } = minify(data);
-	const int = sizer.sync(code);
-	console.log(`> gzip size: ${pretty(int)}`);
+Promise.all([
+	config(pkg.main, 'cjs', {
+		plugins: [resolve, uglify(true)]
+	}),
+	config(pkg.module, 'es', {
+		plugins: [resolve],
+		external: Object.keys(pkg.dependencies)
+	}),
+	config(pkg['umd:main'], 'umd', {
+		name: pkg['umd:name'],
+		plugins: [resolve, buble, uglify(false)]
+	}),
+]).then(_ => {
+	// output gzip size
+	let cjs = readFileSync(pkg.main, 'utf8');
+	let int = sizer.sync(cjs);
+	console.log(`> gzip size: ${ pretty(int) }`);
 });
